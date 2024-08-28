bd2ca68f1b61ebed7fc0e09563edf8dbae58891a
Apalki666 <catalinliviuborzan@gmail.com>
2024-08-28 19:16:04 +0300

    A: Ch2:Player / 2: Game Loop

---
+++ main.py
@@ -0,0 +1,23 @@
+import pygame
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH, ASTEROID_KINDS, ASTEROID_MAX_RADIUS, ASTEROID_MIN_RADIUS, ASTEROID_SPAWN_RATE
+
+print("Starting asteroids!")
+print(f"Screen width: {SCREEN_WIDTH}")
+print(f"Screen height: {SCREEN_HEIGHT}")
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        
+        screen.fill((0, 0, 0))
+        pygame.display.flip()
+
+
+
+if __name__ == "__main__":
+    main()
